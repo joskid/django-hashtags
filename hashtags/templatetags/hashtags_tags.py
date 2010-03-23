b80bd2ddac891ecfe7fe744571dbb4244146d840
Guilherme Gondim <semente@taurinus.org>
2010-03-22 21:23:40 -0300

    Minor change.

--- hashtags/templatetags/hashtags_tags.py
+++ hashtags/templatetags/hashtags_tags.py
@@ -35,8 +35,7 @@ def urlize_hashtags(value):
       {{ value|urlize_hashtags }}
 
     If value is "This is a #test.", the output will be "This is a
-    <a href="[reversed url for hashtagged_item_list(request, hashtag='test')]">
-    #test</a>.".
+    <a href="[reversed url for hashtagged_item_list(request, hashtag='test')]">#test</a>.".
 
     Note that if ``urlize_hashtags`` is applied to text that already contains
     HTML markup, things won't work as expected. Prefer apply this filter to
